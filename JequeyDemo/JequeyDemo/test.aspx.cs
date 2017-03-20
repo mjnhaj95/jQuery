d0a6a9d3a6f5b9ae0fb223a3beec4fc1beaf7369
HAI-PC\HAI <mjnhaj95@gmail.com>
2017-03-20 23:17:38 +0700

    prepend and append trong JS

---
+++ JequeyDemo/JequeyDemo/test.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JequeyDemo
+{
+    public partial class test : System.Web.UI.Page
+    {
+        public class Emplyee
+        {
+            public string firstName { set; get; }
+            public string lastName { set; get; }
+            public string email { set; get; }
+            public int age { set; get; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string json = "[{\"firstName\":\"CDG\",\"lastName\":\"App\",\"email\":\"cdg@gmail.com\",\"age\":14},{\"firstName\":\"John\",\"lastName\":\"Key\",\"email\":\"john@gmail.com\",\"age\":16}]";
+
+            JavaScriptSerializer javaScriptSeralizer = new JavaScriptSerializer();
+
+            var listEmployee = javaScriptSeralizer.Deserialize<List<Emplyee>>(json);
+
+        }
+    }
+}
